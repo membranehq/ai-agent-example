7555d68f7063de10f1548267dd1efc38b97ac396
Jude Agboola <marvinjudehk@gmail.com>
2025-06-03 15:34:23 +0200

    use integration app MCP SERVER URL from env

--- lib/integration-app/getTools.ts
+++ lib/integration-app/getTools.ts
@@ -4,7 +4,15 @@ export async function getTools({
   token,
   app,
 }: { token: string; app?: string }) {
-  let url = `http://localhost:3000/sse?token=${token}`;
+  const INTEGRATION_APP_MCP_SERVER_HOST =
+    process.env.INTEGRATION_APP_MCP_SERVER_HOST;
+
+  if (!INTEGRATION_APP_MCP_SERVER_HOST) {
+    throw new Error('INTEGRATION_APP_MCP_SERVER_HOST is not set');
+  }
+
+  let url = `${INTEGRATION_APP_MCP_SERVER_HOST}/sse?token=${token}`;
+  console.log('url', url);
 
   if (app) {
     url = `${url}&integrationKey=${app}`;
@@ -17,6 +25,7 @@ export async function getTools({
     },
   });
 
+  // Will throw an error is there are no tools so we need to catch it
   try {
     const tools = await mcpClient.tools();
     return tools;
