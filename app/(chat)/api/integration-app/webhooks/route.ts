10de73e78380543073144ebfa7ba5b0fe69ced17
Jude Agboola <marvinjudehk@gmail.com>
2025-07-04 16:09:09 +0100

    re-index MCP tools on connect /disconnect

--- app/(chat)/api/integration-app/webhooks/route.ts
+++ app/(chat)/api/integration-app/webhooks/route.ts
@@ -2,7 +2,6 @@ import { verifyIntegrationAppToken } from '@/lib/integration-app/verifyIntegrati
 import type { NextRequest } from 'next/server';
 import { z } from 'zod';
 import { getUserById } from '@/lib/db/queries';
-import { removeToolsForAppFromIndex } from '@/lib/pinecone/remove-tools-for-app-from-index';
 import { indexMcpTools } from '@/lib/pinecone/index-user-mcp-tools';
 
 const schema = z.object({
@@ -30,40 +29,28 @@ export async function POST(request: NextRequest) {
 
   const { eventType, data } = requestBody as z.infer<typeof schema>;
 
-  const user = await getUserById(data.connection.user.internalId);
+  const dbUser = await getUserById(data.connection.user.internalId);
 
-  if (!user) {
+  if (!dbUser) {
     return new Response('User not found', { status: 404 });
   }
 
-  const _user = {
-    id: user.id,
-    name: user.name,
-  };
-
   /**
-   * Store and remove tools from index based on event type.
+   * Re-index tools from MCP server when connection is created, deleted or disconnected
+   * This is to make sure that we always have the latest tools in the index
    */
+  const shouldRefreshUserToolsIndex =
+    eventType === 'connection.created' ||
+    eventType === 'connection.deleted' ||
+    eventType === 'connection.disconnected';
 
-  switch (eventType) {
-    case 'connection.created':
-      await indexMcpTools({
-        user: _user,
-        app: data.connection.integration.key,
-      });
-      break;
-    case 'connection.deleted':
-      await removeToolsForAppFromIndex({
-        user: _user,
-        app: data.connection.integration.key,
-      });
-      break;
-    case 'connection.disconnected':
-      await removeToolsForAppFromIndex({
-        user: _user,
-        app: data.connection.integration.key,
-      });
-      break;
+  if (shouldRefreshUserToolsIndex) {
+    await indexMcpTools({
+      user: {
+        id: dbUser.id,
+        name: dbUser.name,
+      },
+    });
   }
 
   return Response.json({ success: true }, { status: 200 });
